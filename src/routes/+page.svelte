e8f07811480c37b28d5d203b09cd9eece2a49eb1
Futami <futamiyarn@gmail.com>
2024-10-01 18:46:41 +0700

    Change some code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	import { scriptStores, originalScripts } from '$lib/stores/scripts.js';
-	import { goto } from '$app/navigation';
+	import { originalScripts } from '$lib/stores/scripts.js';
 	import { get } from 'svelte/store';
 	import { onMount } from 'svelte';
 
+	import TurndownService from 'turndown';
+
 	import ConfigIcon from '$lib/icons/config.svelte';
+
+	// #region variables
 
 	let inputValue = get(originalScripts) || '';
 
@@ -15,13 +18,16 @@
 	let totalMinutes = 0;
 	let wordsPerMinute = 0;
 
-	let WPMScale = 0; // default value
-	let WPSScale = 0; // default value
+	let WPMScale = 0;
+	let WPSScale = 0;
 
 	let includeComma: boolean | null = null;
+	let includeNewline: boolean | null = null;
 
-	// Check if 'window' exists to make sure it's running in the browser
-	if (typeof window !== 'undefined') {
+	// #endregion
+
+	// #region local storage
+	function localStorageFunc() {
 		WPMScale = localStorage.getItem('WPMScale') ? Number(localStorage.getItem('WPMScale')) : 200;
 		WPSScale = localStorage.getItem('WPSScale') ? Number(localStorage.getItem('WPSScale')) : 8;
 		includeComma =
@@ -30,12 +36,22 @@
 				: localStorage.getItem('includeComma') === '0'
 					? false
 					: true;
+
+		includeNewline =
+			localStorage.getItem('includeNewline') === '0'
+				? false
+				: localStorage.getItem('includeNewline') === '1'
+					? true
+					: false;
 	}
 
+	// #endregion
+
 	$: {
-		scriptContent = inputValue.replace(/\[\[.*?\]\]/g, '');
+		const turndownService = new TurndownService();
+		scriptContent = turndownService.turndown(inputValue.replace(/\[\[.*?\]\]/g, ''));
 		characters = scriptContent.length;
-		sentences = (scriptContent.match(/[.!?]+/g) || []).length;
+		sentences = countSentences(scriptContent);
 		words = scriptContent.length < 1 ? 0 : scriptContent.trim().split(/\s+/).length;
 		totalMinutes = words / WPMScale;
 		wordsPerMinute = words / (totalMinutes || 1);
@@ -43,6 +59,27 @@
 		if (WPMScale > 0) localStorage.setItem('WPMScale', WPMScale.toString());
 		if (WPSScale > 0) localStorage.setItem('WPSScale', WPSScale.toString());
 		if (includeComma !== null) localStorage.setItem('includeComma', includeComma ? '1' : '0');
+		if (includeNewline !== null) localStorage.setItem('includeNewline', includeNewline ? '1' : '0');
+	}
+
+	function countSentences(scriptContent: string) {
+		// If the script is empty, return 0 sentences
+		if (scriptContent.length === 0) return 0;
+
+		// If the content has only one word/character, return 1 sentence
+		const wordCount = scriptContent.split(/\s+/).length;
+		if (wordCount === 1) return 1;
+
+		// Count sentences based on punctuation marks
+		let sentenceCount = (scriptContent.match(/[.!?]+/g) || []).length;
+
+		// If there are no punctuation marks but there is content, count as 1 sentence
+		if (sentenceCount === 0) sentenceCount = 1;
+
+		// If the content does not end with a punctuation mark, add 1 to the sentence count
+		if (!/[.!?]$/.test(scriptContent)) sentenceCount += 1;
+
+		return sentenceCount;
 	}
 
 	const slide = () => {
@@ -51,30 +88,51 @@
 			return;
 		}
 
-		let newtext = scriptContent.replace(/\n+/g, ' ');
+		let markdown = scriptContent.replace(/\n+/g, ' [[nl]] ');
+		console.log(inputValue);
 
-		let words = newtext.trim().split(/\s+/);
+		let words = markdown.trim().split(/\s+/);
+		console.log(words);
+
 		let validSentences = [];
 
 		let validSentence = '';
 
 		for (const word of words) {
+			if (word.trim() === '[[nl]]') {
+				if (
+					validSentence.trim() !== '' &&
+					(validSentence.trim().endsWith('.') ||
+						validSentence.trim().endsWith('?') ||
+						validSentence.trim().endsWith('!'))
+				) {
+					validSentences.push(validSentence);
+					validSentence = '';
+				}
+				continue;
+			}
+
 			validSentence += ' ' + word;
 
 			if (validSentence.trim().split(/\s+/).length >= WPSScale) {
-				if (word.includes('.') || word.includes(',') || word.includes('?') || word.includes('!')) {
+				if (word.includes('.') || word.includes('?') || word.includes('!')) {
+					validSentences.push(validSentence);
+					validSentence = '';
+				} else if (word.includes(',') && includeComma) {
 					validSentences.push(validSentence);
 					validSentence = '';
 				}
 			}
 		}
 
+		console.log(validSentences);
+
 		if (validSentences.length === 0) validSentences.push(validSentence);
 
-		// Send valid sentences to the store
-		scriptStores.set(validSentences);
-		originalScripts.set(inputValue);
-		goto('/read');
+		// // Send valid sentences to the store
+		// scriptStores.set(validSentences);
+		// originalScripts.set(inputValue);
+		// goto('/read');
 	};
 
 	let isConfigVisible = false;
@@ -88,9 +146,15 @@
 		isMobile = window.innerWidth < 768; // Adjust this breakpoint as needed
 	}
 
+	function detectionTags() {
+		//
+	}
+
 	onMount(() => {
+		localStorageFunc();
 		updateLayout();
 		window.addEventListener('resize', updateLayout);
+
 		return () => window.removeEventListener('resize', updateLayout);
 	});
 </script>
@@ -116,7 +180,13 @@
 	</section>
 	<div class="editor-content">
 		<section class="editor-text" class:full-width={isMobile}>
-			<textarea class="textarea" bind:value={inputValue} placeholder="Input Script here"></textarea>
+			<div class="editor-controls">
+				<button on:click={detectionTags}> <b>B</b> </button>
+				<button on:click={detectionTags}> <i>I</i> </button>
+				<button on:click={detectionTags}> <u>U</u> </button>
+				<button on:click={detectionTags}> <s>S</s> </button>
+			</div>
+			<div class="textarea" contenteditable="true" id="textarea" bind:innerHTML={inputValue}></div>
 		</section>
 		{#if !isMobile || (isMobile && isConfigVisible)}
 			<section class="editor-config" class:mobile={isMobile}>
@@ -132,6 +202,10 @@
 					<input type="checkbox" bind:checked={includeComma} />
 					<span>Include commas</span>
 				</label>
+				<label class="checkbox">
+					<input type="checkbox" bind:checked={includeNewline} />
+					<span>Include Newline (bypass Min Words)</span>
+				</label>
 			</section>
 		{/if}
 	</div>
@@ -143,18 +217,39 @@
 	}
 
 	.editor-header {
-		@apply z-20 flex w-full flex-col items-center space-y-1 bg-white px-3 py-1 shadow-sm sm:flex-row sm:justify-between sm:space-y-0 dark:bg-gray-800 dark:text-white;
+		// Boxing
+		@apply flex w-full flex-col items-center;
+
+		// Boxing (from Small)
+		@apply sm:flex-row sm:justify-between;
+
+		//spacing
+		@apply space-y-1 px-3 py-1 sm:space-y-0;
+
+		//styling
+		@apply bg-white shadow-sm;
+
+		// Dark mode
+		@apply dark:bg-gray-800 dark:text-white;
 	}
 
 	.editor-content {
-		@apply relative flex w-full flex-grow;
+		@apply relative flex w-full flex-grow overflow-hidden;
 	}
 
 	.editor-text {
-		@apply flex h-full w-full items-center;
+		@apply flex h-full w-full flex-col-reverse md:flex-col;
+
+		.editor-controls {
+			@apply flex items-center justify-center gap-x-1 bg-slate-900/70 p-1;
+
+			button {
+				@apply flex h-8 w-8 items-center justify-center rounded-md bg-slate-700 px-2 py-1 font-semibold text-white hover:bg-slate-500;
+			}
+		}
 
 		.textarea {
-			@apply h-full w-full resize-none border-none bg-gray-100 p-2 outline-none dark:bg-gray-700 dark:text-white;
+			@apply h-full w-full resize-none overflow-y-auto bg-gray-100 p-2 outline-none dark:bg-gray-700 dark:text-white;
 		}
 	}
 
